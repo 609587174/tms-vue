0a385bc8260648ed94732a1ceaf9f4b0dbc0b249
lichunli <lichunli@lichunlideMac-mini.local>
2018-05-12 17:53:46 +0800

    chg: 详情页结构调整

---
+++ src/components/itemList.vue
@@ -0,0 +1,25 @@
+<!-- itemList.vue -->
+<template>
+  <div class="label-list">
+    <label><span class="text-red">* </span>姓名:</label>
+    <div class="detail-form-item">{{userData.name}}</div>
+    <div class="detail-form-item">{{userData.name}}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'itemList',
+  props:['type']
+  data: function() {
+    return {
+      user: { name: "aa" },
+      type:this.type?this.type:'text'
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+
+</style>

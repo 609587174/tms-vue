9b7ac1e5507ff4e75582c456997c01918896fb5e
llp0574 <563179705@qq.com>
2018-06-01 20:49:28 +0800

    Fix css import order

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import vuex from 'vuex'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import vuex from 'vuex'
-import store from './store/indexStore'
+import router from './router'
 import axios from 'axios';
+import store from './store/indexStore'
 import * as pbFunc from './assets/utils';
 import http from './api/index';
 import * as filters from '@/assets/js/filter';
+import App from './App'
+
 /* 全局注册公用filter */
 Object.keys(filters).forEach(key => {
     Vue.filter(key, filters[key])

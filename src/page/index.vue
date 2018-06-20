3e8fe733853eb949dd5534d5d9d0e0fd8c03f922
lichunli <lichunli@lichunlideMac-mini.local>
2018-06-20 15:53:12 +0800

    公司主页

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,242 +1,437 @@
-<!-- index.vue -->
-<style type="text/css" scoped lang="less">
-.st-wraper {
-  background-color: #fff;
+<style scoped lang="less">
+@import '../assets/css/themeStyle.less';
+@import '../assets/css/reset.less';
+.el-container {
+  background: #fff;
 }
 
-.public-wraper {
-  position: relative;
-
-  padding-left: 200px;
+.el-container,
+div,
+span,
+a {
+  font-family: PingFangSC-Light!important;
 }
 
-.st-head-wraper {
-  height: 565px;
-
-  background: url("../assets/img/banner.jpg") no-repeat center center;
-  .st-head-middle {
-    width: 904px;
-    height: 565px;
+.el-header {
+  background: #232F36;
+  height: 64px!important;
+  .bpm-head {
+    width: 1200px;
     margin: 0 auto;
-    .head-nav-wraper {
-      line-height: 80px;
-
-      color: #fff;
-      .st-company-name {
-        font-size: 24px;
-        img {
-          position: relative;
-          top: 12px;
-        }
-      }
-      .head-nav {
-        font-size: 16px;
-        a {
-          color: #fff;
-        }
-      }
-    }
-    .head-login-wraper {
-      .company-slogan {
-        padding-top: 95px;
-
-        color: #fff;
-        .company-slogan-size {
-          font-size: 32px;
-          font-weight: 500;
-          line-height: 80px;
-        }
-      }
-    }
-  }
-}
-
-.business-description {
-  width: 904px;
-  padding-top: 30px;
-  padding-bottom: 40px;
-  margin: 0 auto;
-  .description-item {
-    width: 100%;
-    height: 80px;
-    padding: 0 60px;
-    margin-bottom: 30px;
-    img {
-      width: 140px;
-      height: 80px;
-    }
-    h4 {
-      position: relative;
-
-      height: 32px;
-      padding: 0 10px;
-
-      font-size: 20px;
-      line-height: 32px;
-
-      color: #333;
-
-      span {
-        position: absolute;
-        top: 6px;
-
-        display: inline-block;
-        width: 2px;
-        height: 20px;
-
-        background-color: #30a5ff;
-      }
-    }
-    p {
-      padding: 0 10px;
-
-      font-size: 14px;
-
-      color: #555;
-    }
-    .margin-left-35 {
-      margin-left: 35px;
-    }
-    .margin-right-35 {
-      margin-right: 35px;
-    }
-    h4.border-left-blue span {
-      left: 0;
-    }
-    h4.border-right-blue span {
-      right: 0;
-    }
-  }
-}
-
-.footer {
-  padding: 20px 20px;
-
-  font-size: 13px;
-
-  text-align: center;
-
-  color: #fff;
-  background: #666;
-}
-
-.head-user {
-  margin-left: 50px;
-  color: #606266;
-}
-
-.head-user-no {
-  color: #fff;
-  a {
+    line-height: 64px;
     color: #fff;
+    font-size: 16px;
+    img {
+      margin-top: 13px;
+    }
+    a {
+      color: #fff;
+      font-size: 16px;
+    }
+
+    .link {
+      width: 90px;
+      height: 36px;
+      border: 1px solid #fff;
+      display: inline-block;
+      border-radius: 4px;
+      text-align: center;
+      line-height: 34px;
+      margin-left: 40px;
+      a {
+        display: block;
+      }
+    }
+    .user {
+      display: inline-block;
+      margin-left: 30px;
+      .text-blue {
+        color: #26c6da;
+      }
+      span {
+        padding: 0 10px;
+      }
+    }
+  }
+}
+
+.el-main {
+  padding: 0;
+  .banner {
+    width: 100%;
+    height: 705px;
+    background: url('../assets/img/banner.png')no-repeat 50%;
+    .banner-content {
+      width: 1200px;
+      margin: 0 auto;
+      position: relative;
+      .banner-op {
+        position: absolute;
+        top: 280px;
+        width: 100%;
+        text-align: center;
+        .banner-font {
+          font-size: 32px;
+          font-family: PingFangSC-Light;
+          color: #fff;
+          line-height: 64px;
+          width: 562px;
+          height: 64px;
+          background: url('../assets/img/Rectangle.png')no-repeat;
+          text-align: center;
+          margin: 0 auto;
+        }
+        .banner-btn {
+          margin-top: 102px;
+          button {
+            width: 300px;
+            height: 60px;
+            font-size: 22px;
+          }
+        }
+      }
+    }
+  }
+  .advantage {
+    width: 1200px;
+    margin: 0 auto;
+    height: 708px;
+    padding-bottom: 90px;
+    .advantage-list {
+      height: 618px;
+      width: 100%;
+      .advantage-list-msg {
+        height: 255px;
+        margin-bottom: 15px;
+        background: #F7F8FA;
+        text-align: center;
+        padding: 0 22px;
+        img {
+          margin: 26px 0 22px;
+        }
+        span {
+          display: block;
+          font-size: 24px;
+          color: #555555;
+          margin-bottom: 20px;
+        }
+        div {
+          font-size: 16px;
+          color: #50565C;
+          line-height: 22px;
+          width: 279px;
+          margin: 0 auto;
+          text-align: center;
+        }
+      }
+      .advantage-list-img {
+        img {
+          width: 100%;
+        }
+      }
+    }
+  }
+  .product {
+    width: 100%;
+    height: 670px;
+    background: url('../assets/img/product.png')no-repeat 50%;
+    display: inline-block;
+
+    .product-content {
+      width: 1120px; // height: 220px;
+      margin: 0 auto;
+      .is-animating {
+        // width: 920px;
+        background: #fff;
+      }
+      /deep/ .el-carousel__container {
+        height: 220px;
+        .el-carousel__arrow i {
+          font-size: 24px;
+        }
+        .el-carousel__arrow--left {
+          left: 35px;
+        }
+        .el-carousel__arrow--right {
+          right: 35px;
+        }
+      }
+      .product-list {
+        width: 820px;
+        margin: 0 auto;
+        padding: 0 50px;
+        color: #333;
+        height: 220px;
+        >span {
+          font-size: 24px;
+
+          margin: 48px 0 10px 0;
+          display: inline-block;
+        }
+        >div {
+          width: 512px;
+          line-height: 28px;
+          font-size: 14px;
+        }
+      }
+      .app-one {
+        background: url('../assets/img/app1.png')no-repeat right 30px;
+      }
+      .app-two {
+        background: url('../assets/img/app2.png')no-repeat right 30px;
+      }
+      .app-code {
+        ul {
+          list-style-type: none;
+          text-align: center;
+          margin-top: 74px;
+          li {
+            width: 132px;
+            height: 50px;
+            border-radius: 25px;
+            border: 1px solid #333;
+
+            text-align: center;
+            display: inline-block;
+            margin: 0 5px;
+            font-size: 14px;
+            cursor: pointer;
+            position: relative;
+            >span {
+              line-height: 50px;
+            }
+            &:hover {
+              .code {
+                display: block;
+              }
+            }
+            .code {
+              display: none;
+              position: absolute;
+              z-index: 9;
+              left: -1px;
+              bottom: -55px;
+              width: 134px;
+              height: 160px;
+              background: #fff;
+              >span{
+                line-height: 22px;
+                letter-spacing: 3px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .list-title {
+    font-size: 42px;
+    color: #333;
+    text-align: center;
+    letter-spacing: 3px;
+    margin: 90px auto 56px auto;
+    position: relative;
+    width: 280px;
+    &:before {
+      position: absolute;
+      left: -15px;
+      top: 14px;
+      content: " ";
+      display: block;
+      height: 15px;
+      width: 15px;
+      background: #409EFF;
+      -webkit-transform: rotate(45deg);
+      transform: rotate(45deg);
+
+    }
+    &:after {
+      position: absolute;
+      right: -15px;
+      top: 14px;
+      content: " ";
+      display: block;
+      height: 15px;
+      width: 15px;
+      background: #409EFF;
+      -webkit-transform: rotate(45deg);
+      transform: rotate(45deg);
+
+    }
+  }
+}
+
+.bpm-footer {
+  width: 100%;
+  height: 92px;
+  background: #232F36;
+  text-align: center;
+  display: inline-block;
+  >div {
+    margin-top: 20px;
+    font-size: 14px;
+    color: #E8E8E8;
+    line-height: 26px;
   }
 }
 
 </style>
 <template>
-  <div class="main">
-    <div class="st-wraper">
-      <div class="st-head-wraper">
-        <div class="st-head-middle">
-          <div class="head-nav-wraper clearfix">
-            <div class="st-company-name float-left"><a href="/"><img src="../assets/img/91LNG.png"></a> </div>
-            <div class="float-right head-user">
-              <div v-if="users && users.nick_name">
-                欢迎您：<span>{{users && users.nick_name}}，</span>
-                <router-link :to="{path: '/orders/pickupOrders/ordersList'}" class="text-blue">进入91LNG</router-link>
-                <a href="javascript:void(0);">｜</a>
-                <a href="javascript:void(0);" v-on:click="logout">退出</a>
+  <div>
+    <el-container>
+      <el-header>
+        <div class="bpm-head">
+          <el-row>
+            <!-- <el-col :span="12"><img src="@/assets/img/logo.png" alt=""></el-col> -->
+            <el-col :span="12" class="text-right">
+              <a rel="nofollow" target="_blank" :href="businessAppUrl">司机端App下载</a>
+              <span>｜</span>
+              <a rel="nofollow" target="_blank" :href="driverAppUrl">业务端App下载</a>
+              <div v-if="user&&user.profile" class="user">
+                欢迎您：{{user.profile.nick_name}}，<a class="cursor-pointer text-blue" v-on:click="isLogin">进入91LNG</a>
+                <span>|</span>
+                <a v-on:click="logout" class="cursor-pointer">退出</a>
               </div>
-              <div v-else class="head-user-no">
-                <router-link :to="{path: '/register'}" class="text-blue">注册</router-link>
-                <a href="javascript:void(0);">｜</a>
-                <router-link :to="{path: '/login'}" class="text-blue">登录</router-link>
+              <div class="link" v-else>
+                <!-- <router-link :to="{path: '/register'}" class="text-blue">注册</router-link> -->
+                <router-link :to="{path: '/login'}" @click="goLink">登录</router-link>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-header>
+      <el-main>
+        <div class="banner">
+          <div class="banner-content">
+            <div class="banner-op">
+              <div class="banner-font">清洁 / 高效 / 协同 / 智能</div>
+              <div class="banner-btn">
+                <el-button type="primary" v-on:click="isLogin">开始使用</el-button>
               </div>
             </div>
-            <div class="float-right head-nav">
-              <a rel="nofollow" target="_blank" href="https://www.pgyer.com/driver_pro">驾驶员App下载</a>
-              <!-- <a href="#/login">登录</a> -->
-              ｜
-              <a rel="nofollow" target="_blank" href="https://www.pgyer.com/business_pro">业务员App下载</a>
+          </div>
+        </div>
+        <div class="advantage">
+          <div class="list-title">
+            我们的优势
+          </div>
+          <div class="advantage-content">
+            <el-row :gutter="15">
+              <el-col :span="6">
+                <div class="advantage-list">
+                  <div class="advantage-list-msg">
+                    <img src="@/assets/img/ai.png">
+                    <span>全业务场景协作</span>
+                    <div>通过数据同步与多平台协作，支持需要跨部门、 跨角色、跨权限的各类业务协作场景。</div>
+                  </div>
+                  <div class="advantage-list-img">
+                    <img src="@/assets/img/Bitmap1.png">
+                  </div>
+                </div>
+              </el-col>
+              <el-col :span="6">
+                <div class="advantage-list">
+                  <div class="advantage-list-msg">
+                    <img src="@/assets/img/data.png">
+                    <span>全流程数据分析</span>
+                    <div>业务流程中各类指标、数据、操作记录完整沉 淀，通过大数据处理以BI形式提供商业决策支持。</div>
+                  </div>
+                  <div class="advantage-list-img">
+                    <img src="@/assets/img/Bitmap2.png">
+                  </div>
+                </div>
+              </el-col>
+              <el-col :span="6">
+                <div class="advantage-list">
+                  <div class="advantage-list-msg">
+                    <img src="@/assets/img/search.png">
+                    <span>全业务流程覆盖</span>
+                    <div>针对LNG贸易业务实际场景，对贸易流程整个 生命周期进行建模，从而进行自动化处理、监 控管理和持续调优。</div>
+                  </div>
+                  <div class="advantage-list-img">
+                    <img src="@/assets/img/Bitmap3.png">
+                  </div>
+                </div>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+        <div class="product">
+          <div class="list-title">
+            我们的产品
+          </div>
+          <div class="product-content">
+            <el-carousel :interval="5000" arrow="always">
+              <el-carousel-item>
+                <div class="product-list app-one">
+                  <span>业务端 APP</span>
+                  <div>与91LNG-BPM业务管理系统无缝衔接，业务员可随时随地通过手机下单、跟进装卸车信息、 进行销售单结算，并可查看每日业务汇总数据，贸易业务全流程一手掌控。</div>
+                </div>
+              </el-carousel-item>
+              <el-carousel-item>
+                <div class="product-list app-two">
+                  <span>司机端 APP</span>
+                  <div>与91LNG-TMS运输管理系统无缝衔接，装/卸车信息同步下发至司机、磅单/铅封 及时上传审核、合理规划装卸车路径，协助打造运输环节全透明，运输数据全纪录 的承运体系。</div>
+                </div>
+              </el-carousel-item>
+            </el-carousel>
+            <div class="app-code">
+              <ul>
+                <li>
+                  <span>业务端APP</span>
+                  <div class="code">
+                    <img width="100%" :src="businessCodeImg" alt="">
+                    <span>扫码下载</span>
+                  </div>
+                </li>
+                <li>
+                  <span>司机端APP</span>
+                  <div class="code">
+                    <img width="100%" :src="driverCodeImg" alt="">
+                    <span>扫码下载</span>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
-          <div class="head-login-wraper">
-            <div class="float-left company-slogan">
-              <span class="company-slogan-size">LNG智慧运贸解决方案</span>
-              <br>
-              <span class="f-size-22">清洁/高效/协同/智能</span>
-            </div>
-          </div>
         </div>
-      </div>
-      <div class="business-description">
-        <div class="description-item">
-          <div class="float-left margin-right-35">
-            <img src="../assets/img/banner_1.jpg">
-          </div>
-          <div class="float-left">
-            <h4 class="text-left border-left-blue"><span></span>数据精细管理</h4>
-            <p class="text-right">驾驶员/销售/统计/财务数据实时同步、精确统计</p>
-          </div>
+      </el-main>
+      <el-footer>
+        <div class="bpm-footer">
+          <div>Copyright © 2017 成都浩海通达科技有限公司 - 蜀ICP备17041791号-1
+            <br> QQ：963819358(联系客服) - 邮箱：fangyuan@hhtdlng.com(合作洽谈)</div>
         </div>
-        <div class="description-item clearfix">
-          <div class="float-right margin-left-35">
-            <img src="../assets/img/banner_2.jpg">
-          </div>
-          <div class="float-right">
-            <h4 class="text-right border-right-blue">业务协同推进<span></span></h4>
-            <p>从获取销售需求到物流运输、财务管理实现全站多角色高效协同与跟踪记录</p>
-          </div>
-        </div>
-        <div class="description-item  clearfix">
-          <div class="float-left margin-right-35">
-            <img src="../assets/img/banner_3.jpg">
-          </div>
-          <div class="float-left">
-            <h4 class="border-left-blue"><span></span>车辆智能调度</h4>
-            <p>在确保时效性的前提下，依托业界领先的算法技术计算装货与卸货的最优调度方案</p>
-          </div>
-        </div>
-        <div class="description-item">
-          <div class="float-right margin-left-35">
-            <img src="../assets/img/banner_4.jpg">
-          </div>
-          <div class="float-right">
-            <h4 class="text-right border-right-blue">行业全景洞察<span></span></h4>
-            <p>为管理人员与销售人员提供实时更新的全国LNG价格图表，为制定销售策略提供辅助</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer">
-      <span class="company">Copyright © 2018 成都浩海通达科技有限公司</span>
-      <span class="company"> - 蜀ICP备17041791号-1 </span>
-      <br>
-      <span class="contact"> QQ：963819358(联系客服)</span>
-      <span class="contact"> - 邮箱：fangyuan@hhtdlng.com(合作洽谈)</span>
-    </div>
-  </div>
+      </el-footer>
+    </el-container>
   </div>
 </template>
-<script type="text/javascript">
-import publicHeader from '@/components/publicHeader'
-
+<script>
 export default {
-  components: {
-    publicHeader: publicHeader,
-  },
-  data: function() {
-
+  name: 'index',
+  data() {
     return {
-
-    }
+      businessCodeImg: '', //业务端下载二维码
+      driverCodeImg: '', //司机端下载二维码
+      businessAppUrl: '', //业务端下载链接
+      driverAppUrl: '', //司机端下载链接
+      user: this.pbFunc.getLocalData('user', true)
+    };
   },
   computed: {
-    users: function() {
-      return this.$store.state.common.users;
-    }
+
   },
   methods: {
+    goLink() {
+      this.$router.push({ path: '/login' });
+    },
+    isLogin() {
+      if (this.user&&this.user.profile) {
+        this.$router.push({ path: '/dashborad' });
+      } else {
+        this.$router.push({ path: '/login' });
+      }
+    },
     logout: function() {
       this.$confirm("确定退出?", "提示", {
           confirmButtonText: "确定",
@@ -245,7 +440,6 @@ export default {
         })
         .then(() => {
           this.signOut();
-          this.$emit("logout");
         })
         .catch(() => {});
     },
@@ -257,15 +451,39 @@ export default {
             type: 'success'
           });
           localStorage.clear();
-          this.$store.state.common.users = {};
-          this.$router.push({ path: '/login' });
+          this.user = '';
+          this.$router.push({ path: '/' });
         }
 
       }).catch((err) => {
         this.$message.error('退出失败');
       })
     }
+  },
+  created() {
+    let currentUrl = document.location.href.toString();
+    if (currentUrl.match('xxx.91lng.cn')) {
+      this.businessCodeImg = '';
+      this.driverCodeImg = '';
+      this.businessAppUrl = 'https://www.pgyer.com/business_pro';
+      this.driverAppUrl = 'https://www.pgyer.com/driver_pro';
+    } else if (currentUrl.match('bpm.hhtdlng.com')) {
+      this.businessCodeImg = 'http://www.pgyer.com/app/qrcode/newBusinessDev';
+      this.driverCodeImg = 'http://www.pgyer.com/app/qrcode/newDriverDev';
+      this.businessAppUrl = 'https://www.pgyer.com/newBusinessDev';
+      this.driverAppUrl = 'https://www.pgyer.com/newDriverDev';
+    } else if (currentUrl.match('bpm.91lng.cn')) {
+      this.businessCodeImg = 'http://www.pgyer.com/app/qrcode/newBusinessPrepare';
+      this.driverCodeImg = 'http://www.pgyer.com/app/qrcode/newDriverPrepare';
+      this.businessAppUrl = 'https://www.pgyer.com/newBusinessPrepare';
+      this.driverAppUrl = 'https://www.pgyer.com/newDriverPrepare';
+    } else {
+      this.businessCodeImg = 'http://www.pgyer.com/app/qrcode/newBusinessDev';
+      this.driverCodeImg = 'http://www.pgyer.com/app/qrcode/newDriverDev';
+      this.businessAppUrl = 'https://www.pgyer.com/newBusinessDev';
+      this.driverAppUrl = 'https://www.pgyer.com/newDriverDev';
+    }
   }
-}
+};
 
 </script>

65e2ace2ff1748cd84490b04431fae517bd0d1a7
lichunli <lichunli@lichunlideMac-mini.local>
2018-07-17 19:55:32 +0800

    费用导入运单链接调整修复

--- src/page/statistics/costManage/costImport/costImportList.vue
+++ src/page/statistics/costManage/costImport/costImportList.vue
@@ -218,7 +218,7 @@ export default {
     },
     handleMenuClick(tpye, row) {
       if (tpye === 'waybill') {
-        this.$router.push({ path: `/statistics/costManage/costImport/costImportWaybillDetail/${row.waybill_id}/${row.order_id}` });
+        this.$router.push({ path: `/statistics/costManage/costImport/costImportWaybillDetail/${row.waybill_id}` });
       }
       // else if (tpye === 'edit') {
       //   this.$router.push({ path: `/statistics/business/income/editIncome`, query: { id: row.id } });

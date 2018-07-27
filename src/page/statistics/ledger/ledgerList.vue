7db7e1f45e813d34cbeddda29692234202cd25ee
lichunli <lichunli@lichunlideMac-mini.local>
2018-07-27 15:35:59 +0800

    ws断开重启

--- src/page/statistics/ledger/ledgerList.vue
+++ src/page/statistics/ledger/ledgerList.vue
@@ -37,7 +37,7 @@
     <div class="operation-btn">
       <el-row>
         <el-col :span="20" class="total-data">
-          一共{{tableData.data&&tableData.data.waybill?tableData.data.waybill:0}}单，运费总计{{tableData.data&&tableData.data.waiting_charg?tableData.data.waiting_charg:0}}元，报销费用合计{{tableData.data&&tableData.data.income?tableData.data.income:0}}元，行程外费用合计{{tableData.data&&tableData.data.extra_fee?tableData.data.extra_fee:0}}元
+          一共{{tableData.data&&tableData.data.waybill?tableData.data.waybill:0}}单，运费总计{{tableData.data&&tableData.data.waiting_charges?tableData.data.waiting_charges:0}}元，报销费用合计{{tableData.data&&tableData.data.income?tableData.data.income:0}}元，行程外费用合计{{tableData.data&&tableData.data.extra_fee?tableData.data.extra_fee:0}}元
         </el-col>
         <el-col :span="4" class="text-right" v-if="false">
           <export-button :export-type="exportType" :export-post-data="exportPostData" :export-api-name="'exportLogisticData'"></export-button>

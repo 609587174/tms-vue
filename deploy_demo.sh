2e28be8a9727872151c7090c04b3fd74aec7fe90
tianxiaolong <13568960272@163.com>
2018-08-09 14:31:35 +0800

    添加演示环境发布文件

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "预发测试"
 USER=root
-HOST=39.104.21.131
+HOST=39.104.109.186
 DIR=/home/www/front/tms_front
 echo ${USER}@${HOST}:${DIR}
 rm -rf ./dist && npm run build && rsync -avz --delete dist/ ${USER}@${HOST}:${DIR}

1f9929acc369d40afa105f48dcba81afa965a10f
tianxiaolong <13568960272@163.com>
2018-08-15 15:05:16 +0800

    添加自动化测试的发布文件

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -5,4 +5,4 @@ HOST=39.104.71.159
 DIR=/home/www/front/tms_front
 echo ${USER}@${HOST}:${DIR}
 rm -rf ./dist && npm run build && rsync -avz --delete dist/ ${USER}@${HOST}:${DIR}
-echo "正式发布成功"
+echo "开发环境发布成功"

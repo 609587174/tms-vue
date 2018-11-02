a1799836df4f75b5897083fe6cbe5e4cb5e32686
田小龙 <tianxiaolong@xiaolong.local>
2018-11-02 10:02:45 +0800

    添加新的测试环境

--- src/api/index.js
+++ src/api/index.js
@@ -24,7 +24,7 @@ export const getDomainUrl = function(prefix = '') { //掐指一算五个环境
     domainUrl = `${prefix}ptms.hhtdlng.com`;
   } else if (currentUrl.match('ptms.91lng.cn')) { //预发环境
     domainUrl = `${prefix}ptms.91lng.cn`;
-  } else if (currentUrl.match(`tms.hhtdlng.com`) && !currentUrl.match(`devtms.hhtdlng.com`)) { //测试环境
+  } else if (currentUrl.match(`tms.hhtdlng.com`) && !currentUrl.match(`devtms.hhtdlng.com`) && !currentUrl.match(`vtms.hhtdlng.com`)) { //测试环境
     domainUrl = `${prefix}api.hhtdlng.com/tms`;
   } else if (currentUrl.match(`tms.91lng.cn`) && !currentUrl.match(`ptms.91lng.cn`) && !currentUrl.match(`testtms.91lng.cn`)) { //正式环境
     domainUrl = `${prefix}tms.91lng.cn`;
@@ -32,6 +32,8 @@ export const getDomainUrl = function(prefix = '') { //掐指一算五个环境
     domainUrl = `${prefix}devtms.hhtdlng.com`;
   } else if (currentUrl.match(`testtms.91lng.cn`)) { //开发环境
     domainUrl = `${prefix}testtms.91lng.cn`;
+  } else if (currentUrl.match(`vtms.hhtdlng.com`)) { //开发环境
+    domainUrl = `${prefix}api.hhtdlng.com/tms`;
   } else {
     domainUrl = `${prefix}devtms.hhtdlng.com`; //本地开发环境
   }

9bd5e5dbebc12ac5fadbecf6b7e0eacf3cfb4db0
lichunli <lichunli@lichunlideMac-mini.local>
2018-11-02 09:53:15 +0800

    更改测试api链接

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ export const getDomainUrl = function(prefix = '') { //掐指一算五个环境
   } else if (currentUrl.match('ptms.91lng.cn')) { //预发环境
     domainUrl = `${prefix}ptms.91lng.cn`;
   } else if (currentUrl.match(`tms.hhtdlng.com`) && !currentUrl.match(`devtms.hhtdlng.com`)) { //测试环境
-    domainUrl = `${prefix}tms.hhtdlng.com`;
+    domainUrl = `${prefix}api.hhtdlng.com/tms`;
   } else if (currentUrl.match(`tms.91lng.cn`) && !currentUrl.match(`ptms.91lng.cn`) && !currentUrl.match(`testtms.91lng.cn`)) { //正式环境
     domainUrl = `${prefix}tms.91lng.cn`;
   } else if (currentUrl.match(`devtms.hhtdlng.com`)) { //开发环境

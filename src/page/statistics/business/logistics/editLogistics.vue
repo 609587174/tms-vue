bd2d7f5f06a71a41fd4b95e09c534db3ae03e1ea
lichunli <lichunli@lichunlideMac-mini.local>
2018-11-15 17:22:32 +0800

    气差金额支持负数的填写

--- src/page/statistics/business/logistics/editLogistics.vue
+++ src/page/statistics/business/logistics/editLogistics.vue
@@ -293,7 +293,7 @@ export default {
           { pattern: this.$store.state.common.regular.price.match, message: this.$store.state.common.regular.price.tips, trigger: 'blur' },
         ],
         difference_value: [
-          { pattern: this.$store.state.common.regular.price.match, message: this.$store.state.common.regular.price.tips, trigger: 'blur' },
+          { pattern: this.$store.state.common.regular.accounts.match, message: this.$store.state.common.regular.accounts.tips, trigger: 'blur' },
         ],
         actual_mile: [
           { pattern: this.$store.state.common.regular.mile.match, message: this.$store.state.common.regular.mile.tips, trigger: 'blur' },
